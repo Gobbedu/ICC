74fb26f691921c1f133d067da934d2f581dfb501
gobbedu <gobbolegal@gmail.com>
2022-03-25 10:38:14 -0300

    teste na mao funciona, restante WIP

--- Trab1/testaSL.c
+++ Trab1/testaSL.c
@@ -20,8 +20,95 @@
 #include "Refinamento.h"
 
 #define MAXIT_REFINAMENTO 5
+#define HESS_STEP 1
+#define STR_BUFFER 500
 
-int main() {
+typedef struct {
+    int n;
+    double eps1;
+    double eps2;
+    int iteracao;
+    char funcao[STR_BUFFER];
+    
+    void ***Hf;
+    double *He;
+
+    void **Bf;
+    double *Be;
+}SistNl_t;
+
+SistNl_t *alocaSistNl(unsigned int n){
+    SistNl_t *SnL = (SistNl_t *) malloc(sizeof(SistNl_t));
+
+    if (SnL){
+        SnL->n = n;
+
+        SnL->Hf = (void ***) malloc(sizeof(void **)*n);
+        if (!(SnL->Hf)) {
+            free(SnL);
+            return NULL;
+        }
+        for (int i=0; i < n; ++i)
+            SnL->Hf[i] = (void **) malloc(sizeof(void *)*n);
+
+
+        SnL->He = (double *) malloc(sizeof(double)*n);
+        if (!(SnL->He)) {
+        free(SnL->Hf);
+        free(SnL);
+        return NULL;
+        }
+
+        SnL->Bf = (void *) malloc(sizeof(void)*n);
+        if(!(SnL->Bf)){
+            free(SnL->Hf);
+            free(SnL->He);
+            free(SnL);
+        }
+
+        SnL->Be = (double *) malloc(sizeof(double)*n);
+        if (!(SnL->Be)) {
+        free(SnL->Hf);
+        free(SnL->He);
+        free(SnL);
+        return NULL;
+        }
+    }
+  return SnL;
+}
+
+void liberaSistNl(SistNl_t *SL) {
+  free(SL->Be);
+  free(SL->Bf);
+  free(SL->He);
+  free(SL->Hf);
+  free(SL);
+}
+
+SistNl_t *lerSistNL(void)
+{
+    unsigned int n;
+
+    SistNl_t *SnL = NULL;
+  
+    if (scanf("%d",&n) != EOF) 
+    {  
+        SnL = alocaSistNl(n);
+        if (!SnL) return NULL;
+        
+        scanf("%s", SnL->funcao);
+
+        scanf("%lf", &(SnL->eps1));
+        scanf("%lf", &(SnL->eps2));
+        scanf("%i", &(SnL->iteracao));
+
+    }
+  
+  return SnL;
+}
+
+/* FUNCIONA */
+void testaSnL(void){
     void *f;
 
     char *names[] = { "x1" };
@@ -29,11 +116,12 @@ int main() {
     double X_new[] = { 0 }; 
     double X_old[1];
     double delta[1];
+    double A;
+    double b;
+
     int count = 1;
 
-    f = evaluator_create("7*x1-log(x1)");
-    assert(f);
-
+    f = evaluator_create("7*x1-log(x1)"); assert(f);
 
     // F'(Xi) = J(Xi) ou  parciais numericas
     void *d = evaluator_derivative(f, "x1");
@@ -41,41 +129,113 @@ int main() {
     // HESSIANA
     void *h = evaluator_derivative(d, "x1");
 
-    X_old[0] = 1.0; // X0
+    SistLinear_t *SL = alocaSistLinear(1);
+    SL->n = 1;
+    X_old[0] = 0.1;
 
-    double Dxi1;
+    SL->A[0][0] = 1.0;
+
     int i;
     for( i = 0; i < 20; i++)
     {
-        Dxi1 = evaluator_evaluate(d, 1, names, X_old);
-        if( Dxi1 < 0.1)
+        if( SL->A[0][0] < 0.1 )
             break;            
 
-        // H(Xi)*(delta - Xi) = D(Xi)
-        SistLinear_t *SL = alocaSistLinear(1);
-        SL->n = 1;
-
-        // calcula H(Xi)
+		// x_new = x_old - (Px/Dx); 
+        // deriva tudo mais uma vez
+        // H(Xi)*(delta - Xi) =  - D(Xi)
+        
         SL->A[0][0] = evaluator_evaluate(h, 1, names, X_old);
-        // calcula D(Xi)
-        SL->b[0] = Dxi1;
+        SL->b[0] = - (evaluator_evaluate(d, 1, names, X_old));
+        // SL->b[0] = 0.01;
         
         // calcula w
+        // SL->A * w = SL->b
         eliminacaoGauss(SL, delta);
+
+        printf("A: %f   w: %f  b: %f\n", SL->A[0][0], delta[0], SL->b[0]);
         X_new[0] = X_old[0] + delta[0];
 
         printf("iter %d: Xnew %f  Xold %f   delt %f \n", i, X_new[0], X_old[0], delta[0]);
 
+        X_old[0] = X_new[0];
+
         if( delta[0] < 0.0000001)
             break;
+    }
+    liberaSistLinear(SL);
+    printf("raiz:  %f\n", X_new[0]);
+}
 
-        X_old[0] = X_new[0];
+char ** genNames(int n){
+    char **names = malloc(n * sizeof(char *));
+    for(int i = 0; i < n; i++){
+        names[i] = malloc(2 * sizeof(char));
+
+        names[i][0] = 'x';
+        names[i][1] = i+1+'0';
     }
 
-    printf("delta:  %f\n", X_new[0]);
+    // for(int i = 0; i < n; i++){
+    //     printf("name %i : %s\n", i, names[i]);
+    // }
 
+    return names;
+}
+double *genValues(int n, double init){
+    double *values = malloc(n * sizeof(double));
 
-    // printf("f1(0.1) = %g\n", evaluator_evaluate( f, 1, names, X_new ) );
+    for(int i = 0; i < n; i++)
+        values[i] = init;
+
+    return values;
+}
+
+void **genJacobiana(void *f, int count){
+    void **Jacobiana = malloc(count * sizeof(void *));
+
+    char name[2];
+    name[0] = 'x';
+
+    for(int i = 0; i < count; i++){
+        name[1] = i+1+'0';
+        printf("derivating in %s\n", name);
+        Jacobiana[i] = evaluator_derivative(f, name);
+    }
+
+    return Jacobiana;
+}
+
+int main() {
+    // testaSnL();
+
+    // printf("%i\n", S->n);
+    // printf("%s\n", S->funcao);
+    // printf("%lf\n", S->eps1);
+    // printf("%lf\n", S->eps2);
+    // printf("%i\n", S->iteracao);
+
+    //create
+    //derivate (J)
+    //derivate (H)
+
+    SistNl_t *S;
+    S = lerSistNL();
+
+    void *f = evaluator_create(S->funcao);
+    double *values = genValues(S->n, 1);
+    char **names = genNames(S->n);
+
+    S->Bf = genJacobiana(f, S->n);
+    
+
+    // printf("%f\n", evaluator_evaluate(S->Bf[0], S->n, names, values));
+    // for(int i = 0 ; i < S->n; i++)
+    //     printf("jacob %i : %f\n",i , evaluator_evaluate(S->Bf[i], S->n, names, values));
+    
+
+    
+    return 0;
 }
 
 /*
