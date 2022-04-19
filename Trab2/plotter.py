b045d2a27227d0d82defd563c211e5570f9739ec
gobbedu <gobbolegal@gmail.com>
2022-04-19 15:41:43 -0300

    plot Inexat ok

--- Trab2/plotter.py
+++ Trab2/plotter.py
@@ -1,4 +1,5 @@
 from email.utils import collapse_rfc2231_value
+from cv2 import rotate
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -32,15 +33,16 @@ pyplot[0]: https://matplotlib.org/3.1.1/tutorials/introductory/pyplot.html#sphx-
 """
 
 
-csv_file = 'data/testeInexat.csv'
+csv_file = 'data/timestampInexat.csv'
 metodo = 'Inexato' # inexato ou padrao
-saida_grafico = 'data/testeTempoInexat'
+saida_grafico = 'data/timestampInexat.png'
 quero_ver = False    # se true mostra, se falso baixa em saida grafico
 
 col1 = "Aplicacao_metodo_Newton"
 col2 = "Calculo_Gradiente"
 col3 = "Calculo_Hessiana"
 col4 = "Resolucao_Sistema_Linear"
+
 # diferentes dataframes
 tempo_execucao = pd.read_csv(sep=';', filepath_or_buffer=csv_file)
 # banda_memoria         = pd.read_csv('data/auxiliar.csv')
@@ -59,7 +61,8 @@ plt.ylabel("Tempo de execução")
 plt.title(f'Tempo Execucao Newton {metodo}')                       # nome do grafico gerado, aparece na img dpois
 
 # NAO MUDA DAKI PRA BAIXO
-plt.xticks(tempo_execucao.index, x[:len(tempo_execucao.index)]) # magica em python  
+plt.subplots_adjust(bottom=0.14)
+plt.xticks(tempo_execucao.index, x[:len(tempo_execucao.index)], rotation=50) # magica em python  
 plt.legend(["Total Metodo", "Gradiente", "Hessiana", "Sist. Linear"])
 plt.xlabel("dimensão N da Função de Rosenbrock")
 plt.yscale('log')                                               # especificado no Inexatotrabalho
