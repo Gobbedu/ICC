593bf597c6ab3a3d4be71492b7a9584d73e3351b
gobbedu <gobbolegal@gmail.com>
2022-04-20 21:44:01 -0300

    script teste p/ no_Opt

---
+++ Trab2/executeDINF.sh
@@ -0,0 +1,18 @@
+touch newtonPC.c
+make
+make clear
+
+FUNCAO = 'data/funcoesrosenbrock.dat'
+PREP = 'data/teste.dat'
+RODAR = ${PREP}
+
+METRICA = "L3 L2CACHE FLOPS_DS FLOPS_AVX"
+
+echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+for m in ${METRICA}
+do
+    likwid-perfctr -O -C 3 -g ${m} -m ./newtonPC < ${RODAR} > data/teste${m}.log
+    likwid-perfctr    -C 3 -g ${m} -m ./newtonPC < ${RODAR} > data/teste${m}.dat
+done
+echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+
