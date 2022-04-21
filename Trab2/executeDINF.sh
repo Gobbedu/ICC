315108ee130ce2f213140c84a0f540ea6114a7a6
gobbedu <gobbolegal@gmail.com>
2022-04-20 21:47:39 -0300

    erro script

--- Trab2/executeDINF.sh
+++ Trab2/executeDINF.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 touch newtonPC.c
 make
 make clear
 
-FUNCAO = 'data/funcoesrosenbrock.dat'
-PREP = 'data/teste.dat'
-RODAR = ${PREP}
+FUNCAO="data/funcoesrosenbrock.dat"
+PREP="data/teste.dat"
+RODAR=${PREP}
 
-METRICA = "L3 L2CACHE FLOPS_DS FLOPS_AVX"
+METRICA="L3 L2CACHE FLOPS_DS FLOPS_AVX"
 
 echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
 for m in ${METRICA}
