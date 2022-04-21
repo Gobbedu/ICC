d4e7b270fb8431c0738e10110846228abc45270a
gobbedu <gobbolegal@gmail.com>
2022-04-20 21:43:45 -0300

    script teste p/ noOpt

--- Trab2/executaNaOrval.sh
+++
@@ -1,9 +0,0 @@
-touch newtonPC.c
-make
-make clear
-echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
-
-likwid-perfctr -O -C 3 -g L3 -m ./newtonPC < data/funcoesrosenbrock.dat > data/L3.log
-likwid-perfctr -C 3 -g L3 -m ./newtonPC < data/funcoesrosenbrock.dat > data/L3.dat
-
-echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
